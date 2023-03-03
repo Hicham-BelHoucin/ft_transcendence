142e9cefb812456f8f0a11fd350b798a979940e4
hicham bel houcin <belhoucin.hicham@gmail.com>
2023-03-03 11:02:57 +0100

    add login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,15 +3,15 @@ import { Button } from "../components";
 const Login = () => {
   return (
     <div
-      className="h-screen w-screen flex items-center justify-center flex-col gap-40"
+      className="h-screen w-full flex items-center justify-center flex-col "
       style={{
         backgroundImage: `url('/img/background_image.jpeg')`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
     >
-      <h1 className="text-white text-8xl	">PongPlaza</h1>
-      <Button className=" rounded-xl">
+      <h1 className="text-white lg:text-8xl text-4xl">PongPlaza</h1>
+      <Button className=" rounded-xl mt-40">
         <img src="/img/42.svg" />
         <a href="http://localhost:3000/api/auth/42/callback">
           Login with intranet
