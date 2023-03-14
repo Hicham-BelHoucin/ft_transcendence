6ddc4ea8e4c8684451685d65bfa15ca0e776da41
hicham bel houcin <belhoucin.hicham@gmail.com>
2023-03-14 09:51:32 +0100

    __init_fronend__repo with next and tailwind css

---
+++ backend/src/pong/pong.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PongController } from './pong.controller';
+import { PongService } from './pong.service';
+import { PongGateway } from './pong.gateway';
+
+@Module({
+  controllers: [PongController],
+  providers: [PongService, PongGateway],
+  // exports: [PongService],
+})
+export class PongModule {}
--- frontend/src/components/divider/index.tsx
+++
@@ -1,7 +0,0 @@
-const Divider = () => {
-  return (
-    <hr className="w-full h-px bg-gray-200 border-0 rounded dark:bg-gray-700" />
-  );
-};
-
-export default Divider;
