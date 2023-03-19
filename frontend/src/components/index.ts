3d5572c8db2e2304b618f4ad8d3aa368806f27ab
hicham bel houcin <oubakrimo70@gmail.com>
2023-03-19 14:52:22 +0000

    add divider component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Avatar } from "./avatar";
+export { default as Card } from "./card";
+export { default as AvatarGroup } from "./avatarGroup";
+export { default as Divider } from "./divider";
