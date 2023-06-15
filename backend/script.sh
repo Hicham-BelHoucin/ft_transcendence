c6463104359b2dea6fcd160267e57da0997d7ffd
Soufiane El marsi <sel-mars@e2r2p12.1337.ma>
2023-06-15 17:41:25 +0100

    resolved docker issues

--- backend/script.sh
+++ backend/script.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
-cp -r /cache/node_modules/ /app/node_modules/
+echo "Installing dependencies"
+npm install
+
+echo "Initing database and running migrations"
 npx prisma migrate dev --name init
 npx prisma generate
 npx prisma db push --accept-data-loss
 npx prisma studio -b none &
+
+echo "Starting server"
 npm run start:dev
