dda90851dd47521415d1409b12c99d1c7f22d1af
Soufiane El marsi <sel-mars@e2r2p13.1337.ma>
2023-08-05 17:17:38 +0100

    tfa and complete info in landing page along with login and register

--- frontend/src/context/app.context.tsx
+++ frontend/src/context/app.context.tsx
@@ -7,110 +7,97 @@ import { Spinner } from "@/components";
 import { redirect, usePathname } from "next/navigation";
 
 export interface IAppContext {
-  user: IUser | undefined;
-  loading: boolean;
-  authenticated: boolean;
-  fetchUser: () => Promise<void>;
-  updateUser: () => Promise<void>;
+	user: IUser | undefined;
+	loading: boolean;
+	authenticated: boolean;
+	setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+	fetchUser: () => Promise<void>;
+	updateUser: () => Promise<void>;
 }
 
 export const AppContext = React.createContext<IAppContext>({
-  user: undefined,
-  loading: true,
-  authenticated: false,
-  fetchUser: async () => { },
-  updateUser: async () => { },
+	user: undefined,
+	loading: true,
+	authenticated: false,
+	setAuthenticated: () => {},
+	fetchUser: async () => {},
+	updateUser: async () => {},
 });
 
 export const fetcher = async (url: string) => {
-  const accessToken = window.localStorage.getItem("access_token"); // Replace with your actual access token
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_BACK_END_URL}${url}`,
-    {
-      withCredentials: true,
-      // headers: {
-      //   Authorization: `Bearer ${accessToken}`,
-      // },
-    }
-  );
-  return response.data;
+	const response = await axios.get(`${process.env.NEXT_PUBLIC_BACK_END_URL}${url}`, {
+		withCredentials: true,
+	});
+	return response.data;
 };
 
-const AppProvider = ({ children }: {
-  children: React.ReactNode,
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+	const [data, setData] = useState<IUser | undefined>(undefined);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-}) => {
-  // const router = useRouter();
-  const [data, setData] = useState<IUser | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+	const fetchUser = useCallback(async () => {
+		if (isAuthenticated) return;
+		try {
+			setIsLoading(true);
+			const data = await fetcher("api/auth/42");
+			if (data) {
+				setData(data);
+				if (!data.twoFactorAuth) {
+					setIsAuthenticated(true);
+				}
+			}
+		} catch (error) {
+			setIsAuthenticated(false);
+		}
+		setIsLoading(false);
+	}, [isAuthenticated]);
 
-  const fetchUser = useCallback(async () => {
-    if (isAuthenticated) return;
-    try {
-      setIsLoading(true);
-      const data = await fetcher("api/auth/42");
-      if (data)
-        setIsAuthenticated(true);
-      setData(data);
-    } catch (error) {
-      setIsAuthenticated(false);
-    }
-    setIsLoading(false);
-  }, [isAuthenticated]);
+	const updateUser = useCallback(async () => {
+		try {
+			const data = await fetcher("api/auth/42");
+			// const { data } = useSWR("api/auth/42", fetcher, { refreshInterval: 1000 });
+			setData(data);
+		} catch (error) {
+			setIsAuthenticated(false);
+		}
+	}, []);
 
-  const updateUser = useCallback(async () => {
-    try {
-      const data = await fetcher("api/auth/42");
-      // const { data } = useSWR("api/auth/42", fetcher, { refreshInterval: 1000 });
-      setData(data);
-    } catch (error) {
-      setIsAuthenticated(false)
-    }
-  }, []);
+	useEffect(() => {
+		fetchUser();
+		const handleLocalStorageChange = async () => {
+			await updateUser();
+		};
+		window.addEventListener("storage", handleLocalStorageChange);
+		// const id = setInterval(async () => {
+		//   await updateUser();
+		// }, 1000);
+		return () => {
+			// clearInterval(id);
+			window.removeEventListener("storage", handleLocalStorageChange);
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [fetchUser]);
 
-  useEffect(() => {
-    fetchUser();
-    const handleLocalStorageChange = async () => {
-      await updateUser();
-    };
-    window.addEventListener("storage", handleLocalStorageChange);
-    // const id = setInterval(async () => {
-    //   await updateUser();
-    // }, 1000);
-    return () => {
-      // clearInterval(id);
-      window.removeEventListener("storage", handleLocalStorageChange);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchUser]);
+	const path = usePathname();
 
-  const path = usePathname();
+	// console.log(path)
 
-  // console.log(path)
+	if (!isAuthenticated && path !== "/") redirect("/");
 
-  if (!isAuthenticated && path !== "/")
-    redirect("/");
+	// if (isLoading && path !== "/")
+	//   return <Spinner />;
 
-  // if (isLoading && path !== "/")
-  //   return <Spinner />;
+	const appContextValue: IAppContext = {
+		user: data,
+		loading: isLoading,
+		authenticated: isAuthenticated,
+		setAuthenticated: setIsAuthenticated,
+		fetchUser,
+		updateUser,
+	};
 
-
-  const appContextValue: IAppContext = {
-    user: data,
-    loading: isLoading,
-    authenticated: isAuthenticated,
-    fetchUser,
-    updateUser,
-  };
-
-
-
-  return (
-    <AppContext.Provider value={appContextValue}>
-      {children}
-    </AppContext.Provider>
-  );
+	return <AppContext.Provider value={appContextValue}>{children}</AppContext.Provider>;
 };
 
 export default AppProvider;
