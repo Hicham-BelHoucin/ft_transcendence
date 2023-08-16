ba2976683160fbf41fc4ec8d1490571713e91bcc
Soufiane El marsi <sel-mars@e3r2p4.1337.ma>
2023-08-16 17:23:13 +0100

    save data

--- backend/script.sh
+++ backend/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Initing database and running migrations"
-npx prisma migrate dev --name init
+npx prisma migrate --name init
 npx prisma generate
 npx prisma db push --accept-data-loss
-npx prisma studio -b none &
